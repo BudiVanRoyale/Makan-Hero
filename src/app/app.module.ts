d6eed840dc67e708dd0d9c1e0698a676b06d80df
Budi Syahiddin <budisyahiddin@protonmail.ch>
2017-06-10 22:29:09 +0800

    Added stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,61 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FeedComponent} from './components/feed/feed.component';
+import {DetailsComponent} from './components/details/details.component';
+import {LoginComponent} from './components/login/login.component';
+import {PostComponent} from './components/post/post.component';
+import {RegisterComponent} from './components/register/register.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {RouterModule} from "@angular/router";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedComponent,
+    DetailsComponent,
+    LoginComponent,
+    PostComponent,
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: LoginComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      {
+        path: 'profile/:id',
+        component: ProfileComponent
+      },
+      {
+        path: 'post',
+        component: PostComponent
+      },
+      {
+        path: 'feed',
+        component: FeedComponent
+      },
+      {
+        path: 'details/:id',
+        component: DetailsComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

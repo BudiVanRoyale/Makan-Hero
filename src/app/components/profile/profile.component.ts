9cf2bf15ef6fc426e5de34bcf1f8a4f82b73d4ad
Budi Syahiddin <budisyahiddin@protonmail.ch>
2017-06-11 13:38:41 +0800

    komplete profile pij

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -14,6 +14,11 @@ export class ProfileComponent implements OnInit {
 
   private url = 'http://192.168.1.167:8000/api/v1/me';
   private email: string;
+  private pp: string = "https://lh6.googleusercontent.com/-C8EOOPgV2jA/VKhomirnV-I/AAAAAAAAAAs/9iynth2mnmo/superman-facebook.jpg";
+  private name: string;
+  private points: string;
+  private saved: string;
+  private shared: string;
 
   constructor(private router: Router, private http: Http, private ts: toast) {
     this.getUserData();
@@ -36,7 +41,11 @@ export class ProfileComponent implements OnInit {
       });
     const options = new RequestOptions({headers: headers});
     this.http.get(this.url, options).map(res => res.json()).subscribe(msg => {
-      this.email = msg.data.email;
+      this.email = msg.user.email;
+      this.name = msg.user.name;
+      this.points = msg.user.points;
+      this.saved = msg.user.saved;
+      this.shared = msg.user.offered;
     }), err => this.ts.showToast('Error b0ss');
   }
 

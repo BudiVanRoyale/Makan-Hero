eb9013765eecfade71d211ac6e94a0792e972bd3
tengkuhafidz <tengkuhafidz@gmail.com>
2017-06-11 13:03:24 +0800

    styles

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -8,6 +8,9 @@ import { Router }from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
 
+
+  private pp: string = "https://lh6.googleusercontent.com/-C8EOOPgV2jA/VKhomirnV-I/AAAAAAAAAAs/9iynth2mnmo/superman-facebook.jpg";
+
   constructor(private router:Router) { }
 
   ngOnInit() {
